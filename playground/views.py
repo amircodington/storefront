9f129fa655acbbdc6f097eb70bcefaf62f7060e2
amircodington <amircodington@gmail.com>
2021-10-29 21:15:51 +0330

    train

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from django.db.models import Q, F
+from store.models import Product
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    queryset = Product.objects.order_by('unit_price', '-title'),reversed()
+    return render(request, 'hello.html',
+    {'name': 'Amir', 'products': list(queryset)})
