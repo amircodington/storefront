7eb215021651870214ddcbccddcc3877de2acb5e
amircodington <amircodington@gmail.com>
2021-10-29 20:23:53 +0330

    Run again

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'storefront',
         'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'MyPassword'
+        'USER': 'postgres',
+        'PASSWORD': '1234@qweR'
     }
 }
 
